89d7e907ea5ffe449147337a2d05f9a95cfe12a7
ChinmayNoob <chinmaysk0408@gmail.com>
2024-12-24 18:59:13 +0530

    removing contact form and fixing nav

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -76,7 +76,7 @@ export const FloatingNav = ({
           </Link>
         ))}
         <Link
-          href="https://chinmayy.me"
+          href="/"
           className="border text-lg font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full"
         >
           <span className="block sm:hidden">
